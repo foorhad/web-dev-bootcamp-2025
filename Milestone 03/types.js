fc0af188024005dc6d23f81454d0182dd68cd849
foorhad <forhadkhan20330@gmail.com>
2025-07-30 23:51:42 +0600

    added mathmatics operations

---
+++ Milestone 03/types.js
@@ -0,0 +1,12 @@
+var name = "forhad";
+console.log(name);
+
+const year=23;
+console.log(year);
+
+var isstudent = true;
+console.log(typeof isstudent);
+
+console.log("========================================")
+let br = parseInt('for');
+console.log(br);
---
+++ Milestone 03/variable.js
@@ -0,0 +1,4 @@
+var age =13;
+console.log(age);
+let year=989;
+console.log(year);
