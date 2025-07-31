953bffeb5d35814cbc3fbf1f041b58b40f190e50
foorhad <forhadkhan20330@gmail.com>
2025-07-31 21:22:35 +0600

    updated

--- Milestone 03/mathmatics.js
+++ Milestone 03/mathmatics.js
@@ -1,5 +1,28 @@
 let a = 12;
 let b = 2;
 let sum = a+b;
-let sub=a-b, division = a/b, mul=a*b;
-console.log(sub,sum,division,mul);
+let sub=a-b, division = a/b, mul=a*b, rem=a%b;
+console.log(sub,sum,division,mul, rem);
+
+console.log("=======================================================");
+let d="12";
+let e = 2;
+console.log(d*e);
+
+console.log("********************************************");
+let z=Number("200");
+console.log(z);
+console.log("******************************************** float value");
+let nums=4.5, numt=2.778;
+let numsz = nums + numt;
+console.log(numsz.toFixed(2));
+let vals;
+console.log(isNaN(vals));
+
+console.log("******************************************** float value");
+const y = isNaN('12');
+console.log(y);
+const x = isNaN(1 -'12');
+console.log(x);
+console.log(x);
+console.log(x);
