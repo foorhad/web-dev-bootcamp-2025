afa0d503799ff121a2e6da5b70292f9b778c3d9f
foorhad <forhadkhan20330@gmail.com>
2025-08-03 00:21:45 +0600

    explore conditional if else etc

--- Milestone 03/day 2/comparison.js
+++ Milestone 03/day 2/comparison.js
@@ -6,3 +6,7 @@ console.log(a===b);
 
 let d = 23, c='23';
 console.log(d!==c)
+
+/* grater than equal and less than equal */
+console.log(12>=6);
+console.log(6<=6);
---
+++ Milestone 03/day 2/condition.js
@@ -0,0 +1,9 @@
+let penPrice = 10;
+if(penPrice <= 20)console.log("i will buy pen");
+else console.log("i will buy pencil");
+
+console.log("======================================================");
+let money=2000;
+let ismoney =false;
+if(money >= 2000 || ismoney)console.log("i will go newyork");
+else console.log("i will go Nepal");
