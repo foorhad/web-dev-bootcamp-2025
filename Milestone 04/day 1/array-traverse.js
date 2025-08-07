7072c95cdbd635f7c1b39d37d0b63038bab2fea2
foorhad <forhadkhan20330@gmail.com>
2025-08-07 23:15:51 +0600

    array print reverse

--- Milestone 04/day 1/array-traverse.js
+++ Milestone 04/day 1/array-traverse.js
@@ -1,2 +1,9 @@
 const student = ['a','b','c','cgt','dhaka','rangpur','rajshahi'];
 for(let i=0;i<student.length;i++)console.log(i,'-',student[i]);
+console.log("=========================");
+let i=0;
+while(i<student.length){
+    console.log(student[i]);i++;
+}
+console.log("=========================");
+for(let i=(student.length)-1;i>=0;i--)console.log(i,'-',student[i]);
