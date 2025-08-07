c2dd7577358a2905dd0ffb72629c040a5a92232c
foorhad <forhadkhan20330@gmail.com>
2025-08-07 23:10:35 +0600

    started new milestone4

---
+++ Milestone 04/day 1/array-traverse.js
@@ -0,0 +1,2 @@
+const student = ['a','b','c','cgt','dhaka','rangpur','rajshahi'];
+for(let i=0;i<student.length;i++)console.log(i,'-',student[i]);
---
+++ Milestone 04/day 1/recap.js
@@ -0,0 +1,7 @@
+const students ={
+    name : "forhad",
+    roll:122,
+    reg:43,
+    address:"east perola"
+}
+for(let x in students)console.log(x);
