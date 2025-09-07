8b47bcfc6928a2ce8be52462a8d6c999398a5922
foorhad <forhadkhan20330@gmail.com>
2025-09-08 01:11:00 +0600

    explore events handling

--- Milestone 05/day -3/events.js
+++ Milestone 05/day -3/events.js
@@ -1,1 +1,8 @@
 console.log("external file connected");
+
+ function makeyellow(){
+            document.body.style.backgroundColor = "yellow";
+        }
+        function makeamazing(){
+            document.body.style.backgroundColor = 'pink'
+}
