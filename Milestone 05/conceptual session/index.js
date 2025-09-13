6e57fd3c2ded4f1759a4b3199a89d8ecda6c5968
foorhad <forhadkhan20330@gmail.com>
2025-09-14 00:54:21 +0600

    makes a comment box

--- Milestone 05/conceptual session/index.js
+++ Milestone 05/conceptual session/index.js
@@ -31,11 +31,56 @@ const likeButton = document
   .addEventListener("click", function () {
     const likeCount = document.getElementById("like-cnt");
     console.log(likeCount);
-    // const likeCountstr = likeCount.innerText;
-    // let likeCountInrement = parseInt(likeCountstr);
-    // likeCountInrement++;
+    const likeCountstr = likeCount.innerText;
+    let likeCountInrement = parseInt(likeCountstr);
+    likeCountInrement++;
 
-    // likeCount.innerText = likeCountInrement;
+    likeCount.innerText = likeCountInrement;
     // console.log(likeCountInrement);
     // likeButton.setAttribute("disabled", true);
   });
+const contentSectionMar = document.getElementById("txt-box");
+contentSectionMar.style.marginBottom = "10px";
+document.getElementById("cmnt-btn").addEventListener("click", function () {
+  // pick parent
+  const parentSection = document.getElementById("cmnt-box");
+
+  // pick content
+  const contentSection = document.getElementById("txt-box");
+  const conTent = contentSection.value;
+
+  // create tag when keep the comment
+  const pTag = document.createElement("p");
+  pTag.innerText = conTent;
+
+  pTag.addEventListener("click", function () {
+    pTag.remove();
+  });
+
+  parentSection.append(pTag);
+  // console.log(contentSection);
+
+  // add style to the tag
+  pTag.style.backgroundColor = "green";
+  pTag.style.color = "white";
+  pTag.style.border = "2px";
+  pTag.style.borderRadius = "5px";
+  pTag.style.padding = "10px";
+  pTag.style.marginBottom = "5px";
+
+  // clean comment box after comment
+  contentSection.value = "";
+});
+
+const shareSection = document.getElementById("share-btn");
+shareSection.addEventListener("click", function () {
+  console.log("Share button");
+});
+const parentsSection = document.getElementById("parent-sec");
+shareSection.addEventListener("click", function () {
+  console.log("Parent section");
+});
+const bodySection = document.getElementById("body-section");
+shareSection.addEventListener("click", function () {
+  console.log("body section");
+});
