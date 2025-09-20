94e79d6d6660d5ff2655e7767235833cb77f6fa2
foorhad <forhadkhan20330@gmail.com>
2025-09-20 21:02:53 +0600

    developed login system

--- Milestone 05/day -4/script.js
+++ Milestone 05/day -4/script.js
@@ -1,3 +1,17 @@
-document.getElementById("login-btn").addEventListener("click", function(){
-    
+document.getElementById("login-btn").addEventListener("click", function(e){
+    e.preventDefault()
+    // console.log("dom connected")
+
+    const phoneNumber = 12345678, passNumber = 1234
+    // get phone number and in userInput
+    const userPhone = document.getElementById("phone-num").value
+    const userPhoneConvert = parseInt(userPhone)
+    const userPass = document.getElementById("pass-num").value
+    const userPassConvert = parseInt(userPass)
+
+    if(phoneNumber === userPhoneConvert && passNumber === userPassConvert){
+        window.location.href="./pro.html"
+    }
+    else alert("Invalid")
+   
 })
