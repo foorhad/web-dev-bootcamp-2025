6b372819d4a3c043dd6a2e21414436966f107f5a
foorhad <forhadkhan20330@gmail.com>
2025-09-22 02:19:17 +0600

    developed add money section

--- Milestone 05/day -4/add-money.js
+++ Milestone 05/day -4/add-money.js
@@ -7,15 +7,23 @@ document
 
     const bankAccount = document.getElementById("bank-account").value;
     // console.log(bankAccount);
+    if (bankAccount.length < 9) {
+      alert("invalid bank account");
+      return;
+    }
 
     const addAmount = document.getElementById("add-amnt").value;
     const addAmountConvert = parseInt(addAmount);
+    if (addAmountConvert <= 0) alert("add amount correctly");
 
-    const bankPin = document.getElementById("bank-pin").value;
-
+    const bankPin = parseInt(document.getElementById("bank-pin").value);
+    let accountNum = parseInt(123456789),
+      bankPins = parseInt(1234);
+    // if (bankAccount === accountNum && bankPin === bankPins) {
     const availableBal = document.getElementById("total-amount").innerText;
     const availableBalconvert = parseInt(availableBal);
-    // console.log(totalBankAmount);
+
     const presentAmount = addAmountConvert + availableBalconvert;
     document.getElementById("total-amount").innerText = presentAmount;
+    // } else alert("didn't matched information");
   });
