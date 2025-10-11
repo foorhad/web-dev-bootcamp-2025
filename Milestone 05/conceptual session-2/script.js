4135fa12eeb129971efec7a14d564b511be5c1b5
foorhad <forhadkhan20330@gmail.com>
2025-10-11 20:41:13 +0600

    dynmaic cart section

--- Milestone 05/conceptual session-2/script.js
+++ Milestone 05/conceptual session-2/script.js
@@ -13,5 +13,34 @@ function capitalLetter(str){
 for(let i of titles){
     // i.innerHTML = `<span class="text-pink-600">${capitalLetter(i.innerText)}</span>`;
     // i.innerHTML = `<span class="text-pink-600">We are PH Student</span>`;
-    i.classList.remove("title");
+    // i.classList.remove("title");
 }
+
+function getId(id){
+    return document.getElementById(id); 
+}
+
+getId("btn1").addEventListener("click", function(){
+
+    // price
+    const titles= getId("title").innerText;
+    const priceOfproduct = getId("price").innerText;
+    const currentBalance =  getId("total-money").innerText;
+    const totalPrice = parseInt(priceOfproduct) + parseInt(currentBalance);
+    console.log(totalPrice)
+    getId("total-money").innerText = totalPrice;
+    let cartContainer = getId("cart-container");
+    const newCart = document.createElement("div");
+    newCart.innerHTML = `
+                  <div class="cart1 flex justify-between items-center bg-gray-300 p-4 rounded-sm mb-4">
+            <img src="assets/kitchen-1.png" alt="">
+            <div>
+              <h1 class="font-bold">${titles}</h1>
+              <h1 class="text-gray-600">${priceOfproduct}</h1>
+            </div>
+          </div>
+    `;
+    cartContainer.append(newCart);
+})
+
+//total money
