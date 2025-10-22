e366b02c3c636f846cab4317c3a1eb43ad24a404
foorhad <forhadkhan20330@gmail.com>
2025-10-23 01:40:41 +0600

    function

---
+++ Milestone 06/day 01/function.js
@@ -0,0 +1,4 @@
+function add(a=0,b=0){
+    console.log(a,'',b,'',a+b);
+}
+add(0,23);
--- Milestone 06/day 01/recap.js
+++ Milestone 06/day 01/recap.js
@@ -1,3 +1,9 @@
-const a = 34;
-a = 54
+const a = ['for','had','abdullah','al','forhad']
 console.log(a);
+a.pop()
+a.push('kamil')
+a.push('bangladesh')
+console.log(typeof(a));
+console.log(Array.isArray(a))
+console.log(a)
+
