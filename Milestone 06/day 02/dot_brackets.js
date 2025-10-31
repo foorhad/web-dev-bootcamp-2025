c58f73ad23c517cb3ec4831d0fde69f0cb023613
foorhad <forhadkhan20330@gmail.com>
2025-10-31 19:13:14 +0600

    explore js es6 morethan

---
+++ Milestone 06/day 02/dot_brackets.js
@@ -0,0 +1,11 @@
+const students = {
+    name:'forhad',
+    age:20,
+    1 : 34,
+    gender:'male',
+    sallary:22000
+};
+const nam = 'name'
+console.log("dot bracket:-",students[nam])
+console.log(students[1])
+console.log(students['gender']);
---
+++ Milestone 06/day 02/recap.js
@@ -0,0 +1,10 @@
+//arrow function
+
+const add = () => 'hello saddam';
+console.log(add());
+
+
+//distructing
+const {age=23,sallary=44} = {name:'forhad', age:20, gender:'male', sallary:22000};
+console.log(age);
+console.log(sallary)
