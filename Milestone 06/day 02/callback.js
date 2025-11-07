3bcb43f8e1e0cfe232f71acd145648647a6467ac
foorhad <forhadkhan20330@gmail.com>
2025-11-08 01:24:29 +0600

    explore call back function

---
+++ Milestone 06/day 02/callback.js
@@ -0,0 +1,9 @@
+function add(num){
+    console.log("Number is : ",num);
+}
+function result(fnc){
+    console.log(fnc)
+    let a =54;
+    fnc(a);
+}
+result(add)
---
+++ Milestone 06/day 02/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+   printf("eldfdfdfdl");
+   return 0;
+}
