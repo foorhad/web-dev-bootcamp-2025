2b71eaacf4a8c8236db112baf8c9637b78452500
foorhad <forhadkhan20330@gmail.com>
2025-11-08 11:43:34 +0600

    kd

---
+++ Milestone 06/day 03/empty.js
@@ -0,0 +1,12 @@
+let a = ''
+a = ' '
+a = []
+a = {}
+a = true
+a = false
+if(!a){
+    console.log('not empty')
+}
+else console.log("Empty");
+
+if(a===true)console.log("trully")
---
+++ Milestone 06/day 03/premitive.js
@@ -0,0 +1,8 @@
+let a = 'forhad';
+a = 34;
+a = {name : 'forhad', age:3}
+a = [34,32,88];
+a = false
+a = null
+a = undefined
+console.log(typeof a);
